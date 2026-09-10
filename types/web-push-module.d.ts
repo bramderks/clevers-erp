d82f828d1f060080cfa7d84f6eb65ef57b158dd0
bramderks <bram.derks@outlook.com>
2026-09-10 07:26:52 +0200

    fix: remove implicit any from web-push declaration

--- types/web-push-module.d.ts
+++ types/web-push-module.d.ts
@@ -1,7 +1,7 @@
 declare module "web-push" {
   const webpush: {
-    setVapidDetails: (...args: any[]) => void;
-    sendNotification: (...args: any[]) => Promise<unknown>;
+    setVapidDetails: (...args: unknown[]) => void;
+    sendNotification: (...args: unknown[]) => Promise<unknown>;
   };
 
   export default webpush;
