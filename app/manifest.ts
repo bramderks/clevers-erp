4470776d74e4f434fc78e94a88e47b29407472c8
bramderks <bram.derks@outlook.com>
2026-09-24 18:30:16 +0200

    Laat geïnstalleerde webapp starten in ERP-interface

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,7 +5,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Clevers",
     short_name: "Clevers",
     description: "Clevers medewerkers-app",
-    start_url: "/app",
+    start_url: "/dashboard",
     display: "standalone",
     background_color: "#f1f5f9",
     theme_color: "#0f172a",
