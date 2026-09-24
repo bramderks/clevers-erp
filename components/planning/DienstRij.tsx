d333fd9afdf7f176f826a50b7019fe0dc783b401
bramderks <bram.derks@outlook.com>
2026-09-24 19:15:47 +0200

    Gebruik centrale dienst datum- en tijdformattering

--- components/planning/DienstRij.tsx
+++ components/planning/DienstRij.tsx
@@ -7,20 +7,9 @@ type DienstRijProps = {
   onGewijzigd?: () => void;
 };
 
-function formatteerDatum(datum: string) {
-  return new Intl.DateTimeFormat("nl-NL", {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  }).format(new Date(datum));
-}
+function formatteerDatum(datum: string) {\n  return formatDienstDatum(datum);\n}
 
-function formatteerTijd(datum: string) {
-  return new Intl.DateTimeFormat("nl-NL", {
-    hour: "2-digit",
-    minute: "2-digit",
-  }).format(new Date(datum));
-}
+function formatteerTijd(datum: string) {\n  return formatDienstTijd(datum);\n}
 
 function medewerkerNaam(
   medewerker: NonNullable<
